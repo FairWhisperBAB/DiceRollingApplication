2bef05f93bf8ce0ab77d38981d6682a18a0fddab
benjaminbriggs <bbriggs2002@gmail.com>
2022-09-26 16:34:49 -0500

    Added the vibration feature to roll dice as well another SFX for shaking to roll rather than tapping.

--- app/src/main/java/com/example/dicerollingapplication/D20Roller.java
+++ app/src/main/java/com/example/dicerollingapplication/D20Roller.java
@@ -2,15 +2,23 @@ package com.example.dicerollingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Random;
 
-public class D20Roller extends AppCompatActivity {
+public class D20Roller extends AppCompatActivity implements SensorEventListener {
 
     private ImageView imageViewDice;
     private Random rng = new Random();
@@ -22,9 +30,18 @@ public class D20Roller extends AppCompatActivity {
 
     public MediaPlayer SFX;
 
+    private SensorManager sensorManager;
+    private Sensor accelerometerSensor;
+    private boolean isAccelerometerSensorAvailable, itIsNotFirstTime = false;
+    private float currentX, currentY, currentZ, lastX, lastY, lastZ, xDifference, yDifference, zDifference, shakeTreshHold = 5f;
+
+    private Vibrator vibrator;
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.activity_d20_roller);
 
@@ -36,8 +53,25 @@ public class D20Roller extends AppCompatActivity {
             public void onClick(View v)
             {
                 rollDice();
+                SFX = MediaPlayer.create(D20Roller.this, R.raw.roll2);
+                SFX.start();
             }
         });
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null)
+        {
+            accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            isAccelerometerSensorAvailable = true;
+        }
+        else
+        {
+            isAccelerometerSensorAvailable = false;
+        }
+
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
     }
 
     private void rollDice() {
@@ -58,17 +92,11 @@ public class D20Roller extends AppCompatActivity {
 
                 roll.setText(" ");
 
-                SFX = MediaPlayer.create(D20Roller.this, R.raw.roll2);
-                SFX.start();
-
                 break;
             case 3:
                 imageViewDice.setImageResource(R.drawable.d20_3);
 
                 roll.setText(" ");
-
-                SFX = MediaPlayer.create(D20Roller.this, R.raw.roll2);
-                SFX.start();
 
                 break;
             case 4:
@@ -76,17 +104,11 @@ public class D20Roller extends AppCompatActivity {
 
                 roll.setText(" ");
 
-                SFX = MediaPlayer.create(D20Roller.this, R.raw.roll2);
-                SFX.start();
-
                 break;
             case 5:
                 imageViewDice.setImageResource(R.drawable.d20_5);
 
                 roll.setText(" ");
-
-                SFX = MediaPlayer.create(D20Roller.this, R.raw.roll2);
-                SFX.start();
 
                 break;
             case 6:
@@ -94,17 +116,11 @@ public class D20Roller extends AppCompatActivity {
 
                 roll.setText(" ");
 
-                SFX = MediaPlayer.create(D20Roller.this, R.raw.roll2);
-                SFX.start();
-
                 break;
             case 7:
                 imageViewDice.setImageResource(R.drawable.d20_7);
 
                 roll.setText(" ");
-
-                SFX = MediaPlayer.create(D20Roller.this, R.raw.roll2);
-                SFX.start();
 
                 break;
             case 8:
@@ -112,17 +128,11 @@ public class D20Roller extends AppCompatActivity {
 
                 roll.setText(" ");
 
-                SFX = MediaPlayer.create(D20Roller.this, R.raw.roll2);
-                SFX.start();
-
                 break;
             case 9:
                 imageViewDice.setImageResource(R.drawable.d20_9);
 
                 roll.setText(" ");
-
-                SFX = MediaPlayer.create(D20Roller.this, R.raw.roll2);
-                SFX.start();
 
                 break;
             case 10:
@@ -130,17 +140,11 @@ public class D20Roller extends AppCompatActivity {
 
                 roll.setText(" ");
 
-                SFX = MediaPlayer.create(D20Roller.this, R.raw.roll2);
-                SFX.start();
-
                 break;
             case 11:
                 imageViewDice.setImageResource(R.drawable.d20_11);
 
                 roll.setText(" ");
-
-                SFX = MediaPlayer.create(D20Roller.this, R.raw.roll2);
-                SFX.start();
 
                 break;
             case 12:
@@ -148,17 +152,11 @@ public class D20Roller extends AppCompatActivity {
 
                 roll.setText(" ");
 
-                SFX = MediaPlayer.create(D20Roller.this, R.raw.roll2);
-                SFX.start();
-
                 break;
             case 13:
                 imageViewDice.setImageResource(R.drawable.d20_13);
 
                 roll.setText(" ");
-
-                SFX = MediaPlayer.create(D20Roller.this, R.raw.roll2);
-                SFX.start();
 
                 break;
             case 14:
@@ -166,17 +164,11 @@ public class D20Roller extends AppCompatActivity {
 
                 roll.setText(" ");
 
-                SFX = MediaPlayer.create(D20Roller.this, R.raw.roll2);
-                SFX.start();
-
                 break;
             case 15:
                 imageViewDice.setImageResource(R.drawable.d20_15);
 
                 roll.setText(" ");
-
-                SFX = MediaPlayer.create(D20Roller.this, R.raw.roll2);
-                SFX.start();
 
                 break;
             case 16:
@@ -184,17 +176,11 @@ public class D20Roller extends AppCompatActivity {
 
                 roll.setText(" ");
 
-                SFX = MediaPlayer.create(D20Roller.this, R.raw.roll2);
-                SFX.start();
-
                 break;
             case 17:
                 imageViewDice.setImageResource(R.drawable.d20_17);
 
                 roll.setText(" ");
-
-                SFX = MediaPlayer.create(D20Roller.this, R.raw.roll2);
-                SFX.start();
 
                 break;
             case 18:
@@ -202,17 +188,11 @@ public class D20Roller extends AppCompatActivity {
 
                 roll.setText(" ");
 
-                SFX = MediaPlayer.create(D20Roller.this, R.raw.roll2);
-                SFX.start();
-
                 break;
             case 19:
                 imageViewDice.setImageResource(R.drawable.d20_19);
 
                 roll.setText(" ");
-
-                SFX = MediaPlayer.create(D20Roller.this, R.raw.roll2);
-                SFX.start();
 
                 break;
             case 20:
@@ -224,6 +204,71 @@ public class D20Roller extends AppCompatActivity {
                 SFX.start();
 
                 break;
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+        currentX = sensorEvent.values[0];
+        currentY = sensorEvent.values[1];
+        currentZ = sensorEvent.values[2];
+
+        if (itIsNotFirstTime)
+        {
+            xDifference = Math.abs(lastX - currentX);
+            yDifference = Math.abs(lastY - currentY);
+            xDifference = Math.abs(lastZ - currentZ);
+
+            if ((xDifference > shakeTreshHold && yDifference > shakeTreshHold) ||
+            (xDifference > shakeTreshHold && zDifference > shakeTreshHold) ||
+                    (yDifference > shakeTreshHold && zDifference > shakeTreshHold))
+            {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                {
+                    vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+                }
+                else
+                {
+                    vibrator.vibrate(500);
+
+                }
+
+                rollDice();
+                SFX = MediaPlayer.create(D20Roller.this, R.raw.roll1);
+                SFX.start();
+            }
+        }
+
+        lastX = currentX;
+        lastY = currentY;
+        lastZ = currentZ;
+
+        itIsNotFirstTime = true;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (isAccelerometerSensorAvailable)
+        {
+            sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (isAccelerometerSensorAvailable)
+        {
+            sensorManager.unregisterListener(this);
         }
     }
 }
